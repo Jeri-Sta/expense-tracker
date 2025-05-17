7b543baf0c6de6e3e14d86ab6efbfd0e17c21be0
Jeriel Starosky <jerielstarosky@hotmail.com>
2025-05-17 12:51:03 -0300

    Ajustes ambiente

--- src/main/java/br/com/starosky/expensetracker/security/config/CorsConfig.java
+++ src/main/java/br/com/starosky/expensetracker/security/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package br.com.starosky.expensetracker.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,16 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")       // Permite CORS para todos os endpoints
-                        .allowedOrigins("*")     // Permite qualquer origem (você pode restringir aqui)
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(false); // Ajuste conforme necessidade
+                        .allowCredentials(true); // Ajuste conforme necessidade
             }
         };
     }
