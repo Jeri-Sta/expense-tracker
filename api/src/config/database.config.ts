3ec1bc9e77dfbb9739784e59fb621cdccbca592d
Jeri-Sta <jerielstarosky@hotmail.com>
2025-12-06 11:11:22 -0300

    Correções produção

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -11,5 +11,5 @@ export default registerAs('database', () => ({
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV === 'development',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 }));
