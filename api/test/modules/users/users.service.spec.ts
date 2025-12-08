892698b773e0a7484bf48e733478981eda978a79
Jeri-Sta <jerielstarosky@hotmail.com>
2025-12-08 07:50:52 -0300

    #4 - Padronizado backend

---
+++ api/test/modules/users/users.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../../../src/modules/users/users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../../src/modules/users/entities/user.entity';
+import { Repository } from 'typeorm';
+
+describe('UsersService', () => {
+  let service: UsersService;
+  let repo: Repository<User>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+    repo = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should call repository find', async () => {
+    const findSpy = jest.spyOn(repo, 'find').mockResolvedValue([]);
+    await service.findAll();
+    expect(findSpy).toHaveBeenCalled();
+  });
+});
